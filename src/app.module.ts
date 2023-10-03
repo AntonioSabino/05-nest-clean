9c6a90174d1364e203dcd797b0016503a14d26e6
Antonio Sabino <aslimajr@outlook.com>
2023-10-03 16:47:14 -0300

    controller de criação de conta

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { CreateAccountController } from './controllers/create-account.controller';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, CreateAccountController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
